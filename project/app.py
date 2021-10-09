fceb3438a4745d968c2ee6ee9d088fa05a825ecf
Cloud User <christal.john19@gmail.com>
2021-10-09 13:33:00 +0000

    commit code

---
+++ project/app.py
@@ -0,0 +1,30 @@
+import json
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+def get_json():
+    with open('song_data.json') as _data:
+        json_data = json.load(_data)
+
+    counter = 1
+    '''
+    Adding song_id key_value in the song json
+    '''
+    for item in json_data:
+        item['song_id'] = counter
+        counter += 1
+    return json_data
+
+
+@app.route('/')
+def index():
+    data = get_json()
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
